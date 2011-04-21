da84417d51459652ed570fbba9ffcf4b120f8cb3
huangrui <haidu.java@gmail.com>
2011-04-21 14:10:05 +0800

    import file

---
+++ html/lib/base.js
@@ -0,0 +1,17 @@
+var sea = {
+    object : {
+	Sentance : {
+	    objectID : "",
+	    desc : "",
+	    title : "",
+	    who:{type:"pic-file",url:"aimee1.png"},
+	    next : [{type : ""}]
+	}
+	,Text : {
+	    content : ""
+	}
+	,url : {
+	    url : ""
+	}
+    }
+};
