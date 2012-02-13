59de8c08a11b824cc17cf493bf542fd77915d9ee
huangrui <haidu.java@gmail.com>
2012-02-14 01:36:53 +0800

    code

--- code/service/user/service.go
+++ code/service/user/service.go
@@ -14,19 +14,16 @@ func loginProcess(val string) string {
 	for i := 0; i < len(pramas); i++ {
 		prama := pramas[i];
 		keyValue := strings.Split(prama,"=");
-		//取用户名
 		if keyValue[0] == "um"{
 			username = keyValue[1];
 		}
 		
-		//取密码
 		if keyValue[0] == "pw"{
 			password = keyValue[1];
 		}
 	}
 	
-	return "username:" + username + " and password:" + password;
-
+	return username + " & " + password;
 } 
 
 func main() {
