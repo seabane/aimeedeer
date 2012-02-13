404c3f1ec57f78e24a1583598f3e3c8371f098ea
U-home-PC\home <home@home-PC.(none)>
2012-02-14 00:57:07 +0800

    code

--- code/service/user/service.go
+++ code/service/user/service.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"http"
+    "github.com/hoisie/web.go"
 )
 
-func handler(c *http.Conn, r *http.Request) {
-	fmt.Fprintf(c, "Hi there, I love %s!", r.URL.Path[1:])
-}
+func loginProcess(val string) string { 
+	pramas := strings.Split(val, "&");
+	
+	username := "";
+	password := "";
+	
+	for i := 0; i < len(pramas); i++ {
+		prama := pramas[i];
+		keyValue := strings.Split(prama,"=");
+		//取用户名
+		if keyValue[0] == "um"{
+			username = keyValue[1];
+		}
+		
+		//取密码
+		if keyValue[0] == "pw"{
+			password = keyValue[1];
+		}
+	}
+	
+	return "username:" + username + " and password:" + password;
+
+} 
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":80", nil)
+    web.Get("/user/login/(.*)", loginProcess);
+    web.Run("0.0.0.0:8080");
 }
